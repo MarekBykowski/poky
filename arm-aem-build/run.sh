91dc6dc5c4f64a7e5c4c0fe1de5b0c08ebc3b788
Marek Bykowski <marek.bykowski@gmail.com>
2025-06-15 12:33:37 +0000

    run.sh: use of the updated model and add an IRIS option.
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- arm-aem-build/run.sh
+++ arm-aem-build/run.sh
@@ -1,11 +1,19 @@
-#!/bin/bash 
+#!/bin/bash
 
 pathadd() {
     if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
         PATH="${PATH:+"$PATH:"}$1"
     fi
 }
-pathadd ~/Downloads/Base_RevC_AEM_Fast_Models_11.26/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
+#pathadd ~/Downloads/Base_RevC_AEM_Fast_Models_11.26/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
+pathadd ~/Downloads/FVP_Base_RevC-2xAEMvA_11.29_27_Linux64/Base_RevC_AEMvA_pkg/models/Linux64_GCC-9.3
 echo $PATH
-../../meta-arm/scripts/runfvp --verbose ./tmp/deploy/images/fvp-base/core-image-base-fvp-base.fvpconf
+
+#../../meta-arm/scripts/runfvp --verbose ./tmp/deploy/images/fvp-base/core-image-base-fvp-base.fvpconf
 #../../meta-arm/scripts/runfvp --verbose ./tmp/deploy/images/fvp-base/core-image-base-fvp-base.fvpconf2
+
+if [[ $1 == armds ]]; then
+	../../meta-arm/scripts/runfvp --verbose ./tmp/deploy/images/fvp-base/core-image-minimal-fvp-base.fvpconf -- --iris-server --iris-port 7101 --iris-allow-remote
+else
+	../../meta-arm/scripts/runfvp --verbose ./tmp/deploy/images/fvp-base/core-image-minimal-fvp-base.fvpconf
+fi

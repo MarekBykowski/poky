2c182ec88b98e9c1deb3ae7e46bcf998c419bd02
Marek Bykowski <marek.bykowski@gmail.com>
2025-08-22 17:20:26 +0200

    oe-test: update the helper script
    Updates include:
    - set path to 'meta-cxl' where our tests in contrast to the core ones
    are located
    - update the target and port to reflect qemu running 'b2b' container
    - update functions of the helper script
    - add in a 'misc' function that yields extra misc info on the tests
    structure and runs
    
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- cxl/oe-test_me.sh
+++ cxl/oe-test_me.sh
@@ -1,35 +1,50 @@
 #!/bin/bash
 
+metacxl_dir=~/yocto/meta-cxl
+poky_dir=~/yocto/poky
+
 oeqa_list() {
-cat << 'EOF'
-oe-test runtime --list-tests module /yocto/yocto/meta-cxl/lib/oeqa/runtime/cases
-oe-test runtime --list-tests class /yocto/yocto/meta-cxl/lib/oeqa/runtime/cases
-oe-test runtime --list-tests name /yocto/yocto/meta-cxl/lib/oeqa/runtime/cases
-oe-test runtime --list-tests name /yocto/yocto/meta-cxl/lib/oeqa/runtime/cases /yocto/yocto/poky/meta/lib/oeqa/runtime/cases
+cat << EOF
+oe-test runtime --list-tests module $metacxl_dir/lib/oeqa/runtime/cases
+oe-test runtime --list-tests class $metacxl_dir/lib/oeqa/runtime/cases
+oe-test runtime --list-tests name $metacxl_dir/lib/oeqa/runtime/cases
+oe-test runtime --list-tests module $poky_dir/meta/lib/oeqa/runtime/cases
+oe-test runtime --list-tests class $poky_dir/meta/lib/oeqa/runtime/cases
+oe-test runtime --list-tests name $poky_dir/meta/lib/oeqa/runtime/cases
+oe-test runtime --list-tests name $metacxl_dir/lib/oeqa/runtime/cases $poky_dir/meta/lib/oeqa/runtime/cases
 EOF
 }
 
 oeqa_run() {
-cat << 'EOF'
+cat << EOF
 echo ### Test QEMU ###
-# Run a group of tests
-oe-test runtime --target-type simpleremote --target-ip 127.0.0.1:2223 --server-ip 127.0.0.1 --packages-manifest data/config/manifest --test-data-file data/config/testdata.json /yocto/yocto/meta-cxl/lib/oeqa/runtime/cases --run-tests demo1
+# Run all the tests from module 'demo2'
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests demo2
 # Run a single test
-oe-test runtime --target-type simpleremote --target-ip 127.0.0.1:2223 --server-ip 127.0.0.1 --packages-manifest data/config/manifest --test-data-file data/config/testdata.json /yocto/yocto/meta-cxl/lib/oeqa/runtime/cases --run-tests demo2.DEMO2Test.test_script_fail
-# Run a list of tests
-oe-test runtime --target-type simpleremote --target-ip 127.0.0.1:2223 --server-ip 127.0.0.1 --packages-manifest data/config/manifest --test-data-file data/config/testdata.json /yocto/yocto/meta-cxl/lib/oeqa/runtime/cases --run-tests \
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests demo2.DEMO2Test.test_method_ssh
+# Run a number of tests
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests \
 demo2.DEMO2Test.test_script_fail \
 demo2.DEMO2Test.test_method_ssh
 EOF
 }
 
 oeqa_run_core() {
-cat << 'EOF'
+cat << EOF
 echo ### Test QEMU ###
-oe-test runtime --target-type simpleremote --target-ip 127.0.0.1:2223 --server-ip 127.0.0.1 --packages-manifest data/manifest --test-data-file data/testdata.json /yocto/yocto/poky/meta/lib/oeqa/runtime/cases --run-tests \
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $poky_dir/meta/lib/oeqa/runtime/cases --run-tests \
 ping.PingTest.test_ping \
 ssh.SSHTest.test_ssh
 EOF
 }
 
-echo -e "oeqa_list\noeqa_run"
+misc() {
+cat << EOF
+meta-cxl: $metacxl_dir/lib/oeqa/runtime/cases
+oe-core: $poky_dir/meta/lib/oeqa/runtime/cases
+full help at: oe-test runtime --help
+EOF
+}
+
+echo -e "oeqa_list\noeqa_run\noeqa_run_core"
+echo -e "misc"

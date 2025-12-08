722fce38b4bdf80c21455f09563344559e354007
Marek Bykowski <marek.bykowski@gmail.com>
2025-12-08 13:31:37 +0100

    Update a helper script oe-test_me.sh
    Signed-off-by: Marek Bykowski <marek.bykowski@gmail.com>

--- cxl/oe-test_me.sh
+++ cxl/oe-test_me.sh
@@ -24,15 +24,73 @@ EOF
 
 oeqa_run() {
 cat << EOF
-echo ### Test QEMU ###
+### Run all sanity tests ###
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests sanity
+
+
+### Run all cpdk tests from cpdk_2_tests module ##
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests cpdk_2_tests
+
+
+### Run all BringUpTests from bringup_2_tests ###
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests bringup_2_tests
+
+
+### See dependency ###
+# ssh server must be there, otherwise we cannot ssh
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest1
+
+# we require ssh server dropbear must be on there
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest2
+
+# no ping ran before - tests fails dependency
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest3
+
+# ping ran before - tests executed and passes
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests ping dependency.DependencyTest3
+
+# if ndctl ver. != 88 skip the test
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest4
+EOF
+}
+
+oeqa_run_details() {
+cat << EOF
 # Run all the tests from module 'demo2'
 oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests demo2
+
 # Run a single test
 oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests demo2.DEMO2Test.test_method_ssh
+
 # Run a number of tests
 oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests \
 demo2.DEMO2Test.test_script_fail \
 demo2.DEMO2Test.test_method_ssh
+
+# Run all sanity tests
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests sanity
+
+# Run all cpdk tests from cpdk_2_tests module
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests cpdk_2_tests
+
+# Run all BringUpTests from bringup_2_tests
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests bringup_2_tests
+
+# See dependency
+# ssh server must be there, otherwise we cannot ssh
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest1
+
+# we require ssh server dropbear must be on there
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest2
+
+# no ping ran before - tests fails dependency
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest3
+
+# ping ran before - tests executed and passes
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests ping dependency.DependencyTest3
+
+# if ndctl ver. != 88 skip the test
+oe-test runtime --target-type simpleremote --target-ip b2b:2222 --server-ip b2b --packages-manifest data/config/manifest --test-data-file data/config/testdata.json $metacxl_dir/lib/oeqa/runtime/cases --run-tests dependency.DependencyTest4
 EOF
 }
 
